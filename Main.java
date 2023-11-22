739fe44c351b2faa5650a8bd41670358e49b298a
Denis <chernov-denis21@mail.ru>
2023-11-23 00:24:21 +0300

    updated the code in main and added the output for the second commit, task two.

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Задание 2. Это добавлено для первого коммита из четырёх.");
+        System.out.println("Задание 2. Это добавлено для второго коммита из четырёх.");
     }
 }
